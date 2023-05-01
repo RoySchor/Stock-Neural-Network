93c767c8a6a4a9c30b87898674d84bcbfa3665f5
Roy Schor <70181314+RoySchor@users.noreply.github.com>
2023-05-01 07:52:11 -0400

    I normalized the values and made some other changes

--- Vahalla.py
+++ Vahalla.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas as pd
-from tensorflow.keras.callbacks import LambdaCallback
+# from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM
-from tensorflow.keras.optimizers import RMSprop
-from tensorflow import keras
-from sklearn.preprocessing import MinMaxScaler
+# from tensorflow.keras.optimizers import RMSprop
 
 
 def main():
@@ -19,31 +17,31 @@ def main():
     # Reads in all the data, then slices it to only take the data after the start date
     stockDataFrame = stockDataFrame.loc[start_date_1:end_date_1]
 
-    """
-    # Uncomment this for a graph of the stock value itself
-    minClosePrice = stockDataFrame['Close'].min()
-    maxClosePrice = stockDataFrame['Close'].max()
-    plt.plot(stockDataFrame.index, stockDataFrame['Close'])
-    plt.title("AAPL Stock Value from 2014 to 2016")
-    plt.xlabel("Dates (Year-Month)")
-    plt.ylabel("Stock Values in Points")
-    plt.ylim(float(int(minClosePrice - 5.0)), float(int(maxClosePrice + 5.0)))
-    plt.show()
-    """
+    #### Uncomment this for a graph of the stock value itself
+    # minClosePrice = stockDataFrame['Close'].min()
+    # maxClosePrice = stockDataFrame['Close'].max()
+    # plt.plot(stockDataFrame.index, stockDataFrame['Close'])
+    # plt.title("AAPL Stock Value from 2014 to 2016")
+    # plt.xlabel("Dates (Year-Month)")
+    # plt.ylabel("Stock Values in Points")
+    # plt.ylim(float(int(minClosePrice - 5.0)), float(int(maxClosePrice + 5.0)))
+    # plt.show()
+
+    # This normalizes the Closing prices to be btwn 0-1, based off of the max price
+    stockDataFrame['Close'] = stockDataFrame['Close'].apply(lambda x: x/stockDataFrame['Close'].max())
 
     just_close_vals = stockDataFrame['Close'].values
     raw_training_data = []
     raw_x_train_data = []
     raw_y_train_data = []
     eighty_percent = int(len(just_close_vals) * 0.8)
-    
+
     # This is for the training data which is for the first 80% of the data
     for index in range(5, eighty_percent, 5):
       x_train_vals = just_close_vals[index - 5:index - 1]
       y_train_vals = just_close_vals[index - 1]
 
       raw_training_data.append(x_train_vals)
-      # training_data.append([y_train_vals])
       # Need to add the three 0's, because when we np.array it we do matric multiplication 
       # and we need the dimension to be equal for the multiplication to work
       raw_training_data.append([y_train_vals, 0, 0, 0])
@@ -59,12 +57,11 @@ def main():
       y_test_vals = just_close_vals[index - 1]
 
       raw_test_data.append(x_test_vals)
-      # test_data.append([y_test_vals])
       raw_test_data.append([y_test_vals, 0, 0, 0])
       raw_x_test_data.append(x_test_vals)
       raw_y_test_data.append([y_test_vals])
 
-    # Need to fix the arrays np arrays
+    # Need to reshape the data using np arrays
     training_data = np.array(raw_training_data)
     x_train = np.array(raw_x_train_data)
     y_train = np.array(raw_y_train_data)
@@ -76,6 +73,7 @@ def main():
     # model.add(keras.layers.LSTM(hidden_nodes, input_shape=(window, num_features), consume_less="mem"))
 
     model = Sequential()
+    # Shape is 4 --> 1 as we need 4 input neurons (1 for each close value) outputing 1 neuron (the fifth close value)
     model.add(LSTM(64, input_shape=(4, 1)))
     model.add(Dense(1, activation='relu'))
     model.compile(optimizer='adam', loss='mean_squared_error')
@@ -86,12 +84,14 @@ def main():
     # test_loss = model.evaluate(y_train, y_test, verbose=0)
     # print('Test loss:', test_loss)
 
-    # test_predictions = model.predict(test_data[:-1])
+    # test_predictions = model.predict(y_test)
     # print(test_predictions)
 
     plt.title("Network Results")
-    plt.plot(test_data, label='Test Data')
-    plt.plot(training_data, label='Training')
+    plt.plot(y_test, label='y Test Data')
+    # plt.plot(test_predictions, label='Network Test Predictions')
+    # plt.plot(test_data, label='Test Data')
+    # plt.plot(training_data, label='Training')
     plt.legend()
     plt.show()
 
